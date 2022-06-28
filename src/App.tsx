a928c922f0f7ec2e1e974192f333190e7a4c6938
Винничек Иван Александрович <vinnichek@rit.va>
2022-06-28 10:16:27 +0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+import './style.scss';
+
+function App() {
+  return <div className="divvvvv">{'react setup from scratch without cra testing ts'}</div>;
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new ESLintPlugin({
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+    }),
+  ],
+};
